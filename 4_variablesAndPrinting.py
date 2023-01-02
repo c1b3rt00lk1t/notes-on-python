c217248dcd310cee38e1c91f2262a6a718b981a7
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-01-02 14:26:27 +0100

    Notes on variables and printing.

---
+++ 4_variablesAndPrinting.py
@@ -0,0 +1,30 @@
+
+# The print statement takes parameters and prints them including spaces in between
+print("hello",5, True)
+# At the end of each print statement, by default a \n character is printed
+print("bye")
+print("go to next line\nand continue printing")
+
+# The ending \n can be overwritten as follows
+print(5, True, end="")
+print(5, True, end=" | ")
+
+# The convention in value assignments to variables is to leave a blank space before and after the equal symbol
+x = 3
+y = 'hello'
+num = y
+y = 'bye'
+print(x, y, num)
+
+# #################################################
+# 
+# Rules and conventions for naming variables:
+# - meaningful
+# - start with letter or with underscore
+# - inside the name, numbers and underscore are allowed
+# - variables name are case sensitive
+# - uppercase is by convention reserved for classes
+#
+###################################################
+
+asd
