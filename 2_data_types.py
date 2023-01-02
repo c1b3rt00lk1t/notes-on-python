368be8412b5fe0b701a02214d83c1afb220d360e
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-01-02 13:48:45 +0100

    Examples on python types.

---
+++ 2_data_types.py
@@ -0,0 +1,6 @@
+# Basic types in Python can be int, float, str, bool and None 
+# True and False must be written in Pascal case to be booleans
+
+print("The type of 3 is: ", type(3), ", so you know. Instead, 3.1 is of type: ", type(3.1))
+
+print("The type of 'hello' is: ", type('hello'))
