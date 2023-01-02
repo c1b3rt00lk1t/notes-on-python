30e5bc5d2d3a4eaddd4f2e8ab23c57218e08588b
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-01-02 13:54:53 +0100

    Comments on comments ;)

---
+++ 3_comments.py
@@ -0,0 +1,11 @@
+# There is two types of comment (# starts a single line comment)
+print("hello")  # If the comment is in the same line than code, leave two spaces in between
+
+''' 
+    This is 
+    a multiline
+    comment
+'''
+
+
+# Comments must be only used if they add some value, not commenting things that are obvious or deductible
