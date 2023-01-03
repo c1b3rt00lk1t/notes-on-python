133b9e396b78a27f19e72ed9ce97503ee158c046
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-01-03 20:43:36 +0100

    Notes on arithmetic operators.

---
+++ 6_arithmetic_operators.py
@@ -0,0 +1,35 @@
+
+x = 10.0
+y = 3
+
+# x + y is an expression
+# x or y are operands
+# + is an operator
+result = x + y
+
+print(result)
+
+print(x - y)
+print(x * y)
+print(x ** y)
+
+
+# division always returns a float
+print(x / y)
+
+# integer division is equivalent to Math.floor()
+print(x // y)
+
+# modulus operator
+print(x %  y)
+
+# order of operations -> operator precedence
+# brackets/paranthesis
+# exponents
+# multiplication and division and mod
+# addition/substraction
+
+
+
+# in python True translates in 1 and False in 0
+print(4.5 / True)
